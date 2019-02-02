48c590f7ca0606f1f750911f96c62e4a719c51db
Danny Fritz <dannyfritz@gmail.com>
2019-02-02 09:24:07 -0700

    render teapot

--- src/main.rs
+++ src/main.rs
@@ -2,97 +2,110 @@ use nalgebra_glm as glm;
 
 const SCREEN_WIDTH: u32 = 800;
 const SCREEN_HEIGHT: u32 = 600;
+type Float = f32;
+type Vec3F = glm::TVec3<Float>;
+type Vec4F = glm::TVec4<Float>;
+type Vec3U8 = glm::TVec3<u8>;
+type Mat4F = glm::TMat4<Float>;
+
+//TODO: Use scnee object
+struct Scene {
+    proj: Mat4F,
+    view: Mat4F,
+    vertices: Vec<Vec3F>,
+    indices: Vec<usize>,
+    colors: Vec<Vec3U8>,
+    objects: Vec<Mat4F>,
+}
 
 fn main() {
-    let vertices = vec![
-        glm::vec3(1.0, -1.0, -1.0),
-        glm::vec3(1.0, -1.0, 1.0),
-        glm::vec3(-1.0, -1.0, 1.0),
-        glm::vec3(-1.0, -1.0, -1.0),
-        glm::vec3(1.0, 1.0, -1.0),
-        glm::vec3(1.0, 1.0, 1.0),
-        glm::vec3(-1.0, 1.0, 1.0),
-        glm::vec3(-1.0, 1.0, -1.0),
-    ];
-    let indicies: Vec<usize> = vec![
-        1, 3, 0, 7, 5, 4, 4, 1, 0, 5, 2, 1, 2, 7, 3, 0, 7, 4, 1, 2, 3, 7, 6, 5, 4, 5, 1, 5, 6, 2,
-        2, 6, 7, 0, 3, 7,
-    ];
-    let colors = vec![
-        glm::vec3(0, 0, 1),
-        glm::vec3(0, 1, 0),
-        glm::vec3(0, 1, 1),
-        glm::vec3(1, 1, 1),
-        glm::vec3(1, 0, 1),
-        glm::vec3(1, 1, 0),
-    ];
-    let mut objects = vec![];
-    initialize_scene_objects(&mut objects);
+    let obj =
+        obj::Obj::<obj::SimplePolygon>::load(&std::path::Path::new("./teapot/teapot.obj")).unwrap();
     let near_plane = 0.1;
     let far_plane = 100.0;
-    let eye = glm::vec3(0.0, -3.75, 6.5);
+    let eye = glm::vec3(15.0, 5.0, 10.5);
     let lookat = glm::vec3(0.0, 0.0, 0.0);
     let up = glm::vec3(0.0, 1.0, 0.0);
     let view = glm::look_at(&eye, &lookat, &up);
     let proj = glm::perspective(
-        f64::from(SCREEN_WIDTH / SCREEN_HEIGHT),
+        (SCREEN_WIDTH / SCREEN_HEIGHT) as Float,
         30.0,
         near_plane,
         far_plane,
     );
+    let framebuffer = render(proj, view, obj);
+    framebuffer.save("image.png").unwrap();
+}
+
+fn render(
+    proj: Mat4F,
+    view: Mat4F,
+    obj: obj::Obj<obj::SimplePolygon>,
+) -> image::ImageBuffer<image::Rgb<u8>, Vec<<image::Rgb<u8> as image::Pixel>::Subpixel>> {
     let mut framebuffer = image::ImageBuffer::new(SCREEN_WIDTH, SCREEN_HEIGHT);
     let mut depthbuffer = vec![0.0; SCREEN_WIDTH as usize * SCREEN_HEIGHT as usize];
-    for object in objects {
-        for i in 0..indicies.len() / 3 {
-            let v0 = vertices[indicies[i * 3 + 0]];
-            let v1 = vertices[indicies[i * 3 + 1]];
-            let v2 = vertices[indicies[i * 3 + 2]];
-            let v0_clip = vs(&v0, object, view, proj);
-            let v1_clip = vs(&v1, object, view, proj);
-            let v2_clip = vs(&v2, object, view, proj);
-            let v0_homogen = to_raster(&v0_clip);
-            let v1_homogen = to_raster(&v1_clip);
-            let v2_homogen = to_raster(&v2_clip);
-            let mut m = glm::mat3(
-                v0_homogen.x,
-                v0_homogen.y,
-                v0_homogen.w,
-                v1_homogen.x,
-                v1_homogen.y,
-                v1_homogen.w,
-                v2_homogen.x,
-                v2_homogen.y,
-                v2_homogen.w,
-            );
-            let determinant = glm::determinant(&m);
-            if determinant >= 0.0 {
-                continue;
-            }
-            m = glm::inverse(&m);
-            let e0 = glm::vec3(m[0], m[1], m[2]);
-            let e1 = glm::vec3(m[3], m[4], m[5]);
-            let e2 = glm::vec3(m[6], m[7], m[8]);
-            let c = m * glm::vec3(1.0, 1.0, 1.0);
-            for (x, y, pixel) in framebuffer.enumerate_pixels_mut() {
-                let sample = glm::vec3(f64::from(x) + 0.5, f64::from(y) + 0.5, 1.0);
-                let inside0 = evaluate_edge_function(&e0, &sample);
-                let inside1 = evaluate_edge_function(&e1, &sample);
-                let inside2 = evaluate_edge_function(&e2, &sample);
-                if inside0 && inside1 && inside2 {
-                    let one_over_w = (c.x * sample.x) + (c.y * sample.y) + c.z;
-                    if one_over_w >= depthbuffer[(x + y * SCREEN_WIDTH) as usize] {
-                        depthbuffer[(x + y * SCREEN_WIDTH) as usize] = one_over_w;
-                        let color = colors[indicies[(3 * i)] % colors.len()];
-                        *pixel = image::Rgb([color.x * 255, color.y * 255, color.z * 255]);
+    for object in obj.objects {
+        for group in object.groups {
+            for poly in group.polys {
+                match poly.len() {
+                    3 => {
+                        let v0 = glm::make_vec3::<Float>(obj.position.get(poly.get(0).unwrap().0).unwrap());
+                        let v1 = glm::make_vec3::<Float>(obj.position.get(poly.get(1).unwrap().0).unwrap());
+                        let v2 = glm::make_vec3::<Float>(obj.position.get(poly.get(2).unwrap().0).unwrap());
+                        let v0_clip = vs(&v0, glm::identity(), view, proj);
+                        let v1_clip = vs(&v1, glm::identity(), view, proj);
+                        let v2_clip = vs(&v2, glm::identity(), view, proj);
+                        let v0_homogen = to_raster(&v0_clip);
+                        let v1_homogen = to_raster(&v1_clip);
+                        let v2_homogen = to_raster(&v2_clip);
+                        let mut m = glm::mat3(
+                            v0_homogen.x,
+                            v0_homogen.y,
+                            v0_homogen.w,
+                            v1_homogen.x,
+                            v1_homogen.y,
+                            v1_homogen.w,
+                            v2_homogen.x,
+                            v2_homogen.y,
+                            v2_homogen.w,
+                        );
+                        let determinant = glm::determinant(&m);
+                        if determinant >= 0.0 {
+                            continue;
+                        }
+                        m = glm::inverse(&m);
+                        let e0 = glm::vec3(m[0], m[1], m[2]);
+                        let e1 = glm::vec3(m[3], m[4], m[5]);
+                        let e2 = glm::vec3(m[6], m[7], m[8]);
+                        let c = m * glm::vec3(1.0, 1.0, 1.0);
+                        for (x, y, pixel) in framebuffer.enumerate_pixels_mut() {
+                            let sample = glm::vec3(x as Float + 0.5, y as Float + 0.5, 1.0);
+                            let inside0 = evaluate_edge_function(&e0, &sample);
+                            let inside1 = evaluate_edge_function(&e1, &sample);
+                            let inside2 = evaluate_edge_function(&e2, &sample);
+                            if inside0 && inside1 && inside2 {
+                                let one_over_w = (c.x * sample.x) + (c.y * sample.y) + c.z;
+                                if one_over_w >= depthbuffer[(x + y * SCREEN_WIDTH) as usize] {
+                                    depthbuffer[(x + y * SCREEN_WIDTH) as usize] = one_over_w;
+                                    let alpha = glm::dot(&e0, &sample);
+                                    let beta = glm::dot(&e1, &sample);
+                                    let charlie = glm::dot(&e2, &sample);
+                                    *pixel =
+                                        image::Rgb([(alpha * 20055.0) as u8, (beta * 20055.0) as u8, (charlie * 20055.0) as u8]);
+                                }
+                            }
+                        }
                     }
+                    //_ => println!("{}", poly.len()),
+                    _ => {}
                 }
             }
         }
     }
-    framebuffer.save("image.png").unwrap();
+    framebuffer
 }
 
-fn evaluate_edge_function(e: &glm::TVec3<f64>, sample: &glm::TVec3<f64>) -> bool {
+fn evaluate_edge_function(e: &Vec3F, sample: &Vec3F) -> bool {
     let result = (e.x * sample.x) + (e.y * sample.y) + e.z;
 
     if result > 0.0 {
@@ -113,41 +126,15 @@ fn evaluate_edge_function(e: &glm::TVec3<f64>, sample: &glm::TVec3<f64>) -> bool
     return true;
 }
 
-fn vs(
-    pos: &glm::TVec3<f64>,
-    m: glm::TMat4<f64>,
-    v: glm::TMat4<f64>,
-    p: glm::TMat4<f64>,
-) -> glm::TVec4<f64> {
+fn vs(pos: &Vec3F, m: Mat4F, v: Mat4F, p: Mat4F) -> Vec4F {
     p * v * m * glm::vec4(pos.x, pos.y, pos.z, 1.0)
 }
 
-fn to_raster(v: &glm::TVec4<f64>) -> glm::TVec4<f64> {
+fn to_raster(v: &Vec4F) -> Vec4F {
     glm::vec4(
-        f64::from(SCREEN_WIDTH) * (v.x + v.w) / 2.0,
-        f64::from(SCREEN_HEIGHT) * (v.w - v.y) / 2.0,
+        SCREEN_WIDTH as Float * (v.x + v.w) / 2.0,
+        SCREEN_HEIGHT as Float * (v.w - v.y) / 2.0,
         v.z,
         v.w,
     )
-}
-
-fn initialize_scene_objects(objects: &mut Vec<glm::TMat4<f64>>) {
-    let identity: glm::TMat4<f64> = glm::identity();
-
-    let mut m0 = glm::translate(&identity, &glm::vec3(0.0, 0.0, 2.0));
-    m0 = glm::rotate(&m0, 45.0, &glm::vec3(0.0, 1.0, 0.0));
-
-    let mut m1 = glm::translate(&identity, &glm::vec3(-3.75, 0.0, 0.0));
-    m1 = glm::rotate(&m1, 30.0, &glm::vec3(1.0, 0.0, 0.0));
-
-    let mut m2 = glm::translate(&identity, &glm::vec3(3.75, 0.0, 0.0));
-    m2 = glm::rotate(&m2, 60.0, &glm::vec3(0.0, 1.0, 0.0));
-
-    let mut m3 = glm::translate(&identity, &glm::vec3(0.0, 0.0, -2.0));
-    m3 = glm::rotate(&m3, 90.0, &glm::vec3(0.0, 0.0, 1.0));
-
-    objects.push(m0);
-    objects.push(m1);
-    objects.push(m2);
-    objects.push(m3);
 }
